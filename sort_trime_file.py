30167320d026e1e0b481362d17bb4885e707e3a8
onion <54584047+oniondelta@users.noreply.github.com>
2022-06-29 23:30:58 +0800

    Update sort_trime_file.py

--- sort_trime_file.py
+++ sort_trime_file.py
@@ -291,7 +291,7 @@ shutil.copytree('./trimefiles/實體(藍牙)鍵盤專用/', './sort_trime/實體
 shutil.copytree('./trimefiles/舊312版主程式/', './sort_trime/舊312版主程式/')
 
 
-#適用3.2.6鍵盤檔
+#適用3.2.1鍵盤檔
 shutil.copytree('./trimefiles/3.2.1鍵盤檔/', './sort_trime/3.2.1鍵盤檔/')
 
 
